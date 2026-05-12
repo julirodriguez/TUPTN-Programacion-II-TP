372e0e8812c82e4849b388247612449bee841b05
Picijp <JuliaPicierro2@gmail.com>
2026-05-12 06:00:58 -0300

    Agregue el menu hamburguesa

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   if (window.initPlanner) initPlanner()
   if (window.mostrarCompras) mostrarCompras()
 })
+
+const menuButton = document.getElementById("menu-toggle")
+const sideMenu = document.getElementById("side-menu")
+
+if (menuButton && sideMenu) {
+  menuButton.addEventListener("click", () => {
+    sideMenu.classList.toggle("open")
+  })
+}
